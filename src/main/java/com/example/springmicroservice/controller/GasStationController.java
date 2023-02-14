7253aacd17e4ff49dcf39d1766c386318dbafdfb
Mladen Jovanovski <83095663+makyjovanovsky@users.noreply.github.com>
2023-02-15 00:44:01 +0100

    Update GasStationController.java

--- src/main/java/com/example/springmicroservice/controller/GasStationController.java
+++ src/main/java/com/example/springmicroservice/controller/GasStationController.java
@@ -16,12 +16,14 @@ public class GasStationController {
     public GasStationController(GasStationService gasStationService) {
         this.gasStationService = gasStationService;
     }
-
+    
+    @CrossOrigin(origins = "https://makpetrol.herokuapp.com/")
     @GetMapping(path = "/get-gas-stations", produces = "application/json")
     public List<GasStationEntity> getGasStations(@RequestParam(name = "city") String city) {
         return gasStationService.findAllGasStationsInCity(city);
     }
-
+    
+    @CrossOrigin(origins = "https://makpetrol.herokuapp.com/")
     @GetMapping(path = "/get-all-gas-stations", produces = "application/json")
     public List<GasStationEntity> getAllGasStations() {
         return gasStationService.getAllStations();
