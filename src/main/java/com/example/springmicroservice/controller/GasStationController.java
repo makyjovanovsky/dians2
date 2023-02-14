c53ffa20ff6671c8bdc15ef5be1d836ca7545ffd
makyjovanovsky <mjovanovski04@gmail.com>
2023-02-15 00:38:52 +0100

    final final commit

--- src/main/java/com/example/springmicroservice/controller/GasStationController.java
+++ src/main/java/com/example/springmicroservice/controller/GasStationController.java
@@ -2,6 +2,7 @@ package com.example.springmicroservice.controller;
 
 import com.example.springmicroservice.entity.GasStationEntity;
 import com.example.springmicroservice.service.GasStationService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +18,13 @@ public class GasStationController {
         this.gasStationService = gasStationService;
     }
 
+    @CrossOrigin(origins = "https://makpetrol.herokuapp.com/")
     @GetMapping(path = "/get-gas-stations", produces = "application/json")
     public List<GasStationEntity> getGasStations(@RequestParam(name = "city") String city) {
         return gasStationService.findAllGasStationsInCity(city);
     }
 
+    @CrossOrigin(origins = "https://makpetrol.herokuapp.com/")
     @GetMapping(path = "/get-all-gas-stations", produces = "application/json")
     public List<GasStationEntity> getAllGasStations() {
         return gasStationService.getAllStations();
